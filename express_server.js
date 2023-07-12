1da8ce587be9ecf97404afa458a77e12d707f428
Alex Tamayo <catd00@gmail.com>
2023-07-11 20:51:38 -0700

    when submit redirects to new address

--- express_server.js
+++ express_server.js
@@ -6,7 +6,6 @@ const PORT = 3000; // default port 8080
 app.set("view engine", "ejs");
 
 
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -41,15 +40,23 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString(6);
+  const longURL = req.body["longURL"];
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
-});
+
+// app.get("/u/:id", (req, res) => {
+//   // const longURL = ...
+//   res.redirect(longURL);
+// });
 
 
 // app.get("/urls.json", (req, res) => {
