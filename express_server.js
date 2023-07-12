12d36c42770e8715aebd75c4f52eb0d7446586bf
Alex Tamayo <catd00@gmail.com>
2023-07-11 20:11:54 -0700

    Added the POST and the submit page

--- express_server.js
+++ express_server.js
@@ -12,20 +12,45 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const generateRandomString = function(lenght) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const characterLength = characters.length;
+  for (let i = 0; i < lenght; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characterLength));
+  }
+  return result;
+};
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+// console.log(generateRandomString(6));
+
+
+app.use(express.urlencoded({ extended: true }));
+
+
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 
 // app.get("/urls.json", (req, res) => {
 //   res.json(urlDatabase);
